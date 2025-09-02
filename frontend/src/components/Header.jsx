3a6d3f24201d47bd6a14be70adec6fc0d24923ba
GrimProg <vavilovila01@gmail.com>
2025-09-02 20:40:11 +0600

    Add Home button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import "../style/header.css"
+import HomeButton from "./headerComponents/HomeButton"
 
 export default function Header() {
     return (
         <div className="header-wrapper">
             <h2>My Web-Site</h2>
+            <nav>
+                <HomeButton/>
+            </nav>
         </div>
     )
 }
---
+++ frontend/src/components/headerComponents/HomeButton.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+
+export default function HomeButton() {
+    return (
+        <Link to="/home">Home</Link>
+    )
+}
